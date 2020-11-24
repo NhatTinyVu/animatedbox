2bda56eeada881e6255f93851166c86751c5024d
Nhat Vu <nhattinyvu@gmail.com>
2020-11-24 14:23:38 +0700

    implement ui
    implement ui

--- src/containers/animated-box/AnimatedBox.styles.js
+++ src/containers/animated-box/AnimatedBox.styles.js
@@ -2,10 +2,21 @@ import {StyleSheet} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 export default StyleSheet.create({
+  safeView: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: Colors.lighter,
+  },
+  absoluteBottomContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    padding: 8,
+  },
+  velocity: {
+    textAlign: 'center',
   },
 });
